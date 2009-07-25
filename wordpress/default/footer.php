35e730b9d9d03551a88b706030ce7a259a5a139f
Patrick O'Doherty <patrick@homepc.(none)>
2009-07-25 17:57:23 +0100

    Removed google adsense and analytics javascript from the wordpress theme. Don't need to be forcing ads on people

--- wordpress/default/footer.php
+++ wordpress/default/footer.php
@@ -11,14 +11,5 @@
 </div>
 
 		<?php wp_footer(); ?>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-2393307-2");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
 </body>
 </html>
--- wordpress/default/single.php
+++ wordpress/default/single.php
@@ -53,18 +53,6 @@
 			</div>
 		</div>
 
-<script type="text/javascript"><!--
-google_ad_client = "pub-2143834100197953";
-/* 468x60, created 3/17/09 */
-google_ad_slot = "3421500553";
-google_ad_width = 468;
-google_ad_height = 60;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-
 
 	<?php comments_template(); ?>
 
